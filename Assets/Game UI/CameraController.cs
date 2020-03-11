1e9763fd2b9fc8768fc306658749bf314347adf0
RullDeef <34412244+RullDeef@users.noreply.github.com>
2020-03-11 12:14:28 +0300

    add star material + fixed camera

--- Assets/Game UI/CameraController.cs
+++ Assets/Game UI/CameraController.cs
@@ -90,7 +90,7 @@ public class CameraController : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-                PlanetSystem planetSystem = hit.transform.GetComponent<PlanetSystem>();
+                PlanetSystem planetSystem = hit.transform.parent.GetComponent<PlanetSystem>();
                 if (planetSystem != null)
                 {
                     observablePlanetSystem = planetSystem;
