1e84e904b6d6a9ae9a38ca3d8840cd70546be266
Andrew Bugaenko <andreibugaenko2012@mail.ru>
2020-04-01 22:42:36 +0300

    Added system menu

---
+++ Assets/GameMenuPrefab/GameMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMenuController : MonoBehaviour
+{
+    public Transform camera;
+    public bool isSelected = false;
+    public GameObject menuSystem;
+    public GameObject[] planets;
+    public Text shipText;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.LookAt(camera);
+
+        if(isSelected == false)
+        {
+            menuSystem.SetActive(false);
+        }
+        else
+        {
+            menuSystem.SetActive(true);
+        }
+    }
+}
